2074ed561f4a6fc371da1b6ce7a2ba1b879eae7a
Evgeniya Sukhovaya <C226-88-39-D@yandex.ru>
2020-09-17 21:25:31 +0300

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket_price = 2_560;
+        int bonus_miles = ticket_price / 20;
+        System.out.println(bonus_miles);
+    }
+}
